0a849aa5f21b1492179477b38521a9a9c70bbb09
Ahmed Samady <samadycrm@gmail.com>
2023-05-16 15:51:32 +0100

    Initial commit

---
+++ View.cs
@@ -0,0 +1,24 @@
+﻿// Test class modeling the view
+
+public class View
+{
+
+	internal virtual void updateRank(int rank)
+	{
+		Console.WriteLine("View: Notify rank change: " + rank);
+	}
+
+	internal virtual void updateReward(int reward)
+	{
+		Console.WriteLine("View: Notify reward change: " + reward);
+	}
+
+	internal virtual void updatePositiveScore(int s, int rs)
+	{
+		Console.WriteLine("View: Notify new P score: " + s + ", " + rs);
+	}
+	internal virtual void updateNegativeScore(int s, int rs)
+	{
+		Console.WriteLine("View: Notify new N score: " + s + ", " + rs);
+	}
+}
